caa59db22a6a56bc937a8f7a58226b840d825b64
sbx_user1051 <sbx_user1051@169.254.227.229>
2021-09-18 10:49:39 +0000

    Sat Sep 18 10:49:39 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,24 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def getPrice(item):
+    switcher =  {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15
+    }
+    return switcher.get(item,0)
+    
+
 def checkout(skus):
+    lengthOfSku = len(skus)
+
+    if(lengthOfSku == 1):
+        price = getPrice(skus)
+        return price
+    elif(lengthOfSku > 1):
+        price = getPrice(skus[lengthOfSku - 1])
     raise NotImplementedError()
+
