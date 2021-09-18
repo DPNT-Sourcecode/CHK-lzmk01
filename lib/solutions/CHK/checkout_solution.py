263a03437612f80aea9e2c26709478e83aa10be9
sbx_user1051 <sbx_user1051@169.254.227.229>
2021-09-18 11:07:39 +0000

    Sat Sep 18 11:07:39 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,12 +12,25 @@ def getPrice(item):
     }
     return switcher.get(item,0)
 
-def calculatePrice(item,qty):
+def calculatePrice(item,qty,ppu):
     if(item == 'A'):
         if(qty >= 3):
             mult = qty/3
+            rem = qty%3
             price = mult * 130
-            pro
+            price += rem * ppu
+            return price
+    elif(item == 'B'):
+        if(qty >= 2):
+            mult = qty/2
+            rem = qty%2
+            price = mult * 45
+            price += rem * ppu
+            return price
+    else:
+        return qty*ppu
+
+
 
 def checkout(skus):
     lengthOfSku = len(skus)
@@ -36,6 +49,7 @@ def checkout(skus):
             qty = skus[0:-1]
             return price * qty
     raise NotImplementedError()
+
 
 
 
