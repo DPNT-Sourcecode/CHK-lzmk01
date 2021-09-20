c4fe3756ef9e1ecfa16798e92dfb6a9946f671c1
sbx_user1051 <sbx_user1051@169.254.8.45>
2021-09-20 06:24:52 +0000

    Mon Sep 20 06:24:52 UTC 2021

--- lib/solutions/CHK/itemsModel.py
+++ lib/solutions/CHK/itemsModel.py
@@ -3,5 +3,5 @@ from collections import namedtuple
 BasketItem= namedtuple("Item",["basketSku"])
 
 SuperMarketStockItem =  namedtuple(
-    "StockItem", ["ItemName","ItemPrice","AsActiveDiscount","DiscountPrice","DiscountQunatity"]
+    "StockItem", ["ItemName","ItemPrice","AsActiveDiscount","AsSpecialDscount","DiscountPrice","DiscountQuantity","SpecialDscountName"]
 )
